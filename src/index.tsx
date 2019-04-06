a62120f843c353d9de632914df2e1445af70bfe4
Marco <l.__marco@hotmail.de>
2019-04-06 22:29:05 +0200

    finally setup is now fine. added babel as typscript/react transpiler

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import './styles.scss';
+import * as React from 'react';
+import App from './components/app/app';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
