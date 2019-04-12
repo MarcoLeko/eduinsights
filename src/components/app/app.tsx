f5a28ce6f9559344210032fe762dcda0d73cc1a7
Marco <l.__marco@hotmail.de>
2019-04-12 19:28:30 +0200

    Svgs can now be imported as React components split chunks correctly set and started with service worker config

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import './app.scss';
-import logo from '../../assets/logo.svg';
+import {ReactComponent as Logo } from '../../assets/logo.svg';
 
 export class App extends React.Component {
 
     public render(): React.ReactNode {
         return (
             <div className="app">
-                <img src={logo} className="app-logo" alt="logo"/>
+                <Logo className="app-logo"/>
             </div>
         );
     }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import './styles.scss';
-import * as React from 'react';
+import React from 'react';
 import App from './components/app/app';
 import * as ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
+
+serviceWorker.unregister();
