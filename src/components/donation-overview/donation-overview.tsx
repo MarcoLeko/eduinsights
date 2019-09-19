b4065674295fbf6bbb3d959216fb60af9b9a391a
Marco <l.__marco@hotmail.de>
2019-09-19 20:00:27 +0200

    made it work - navbar-logo now transitions on scroll

--- src/components/donation-overview/donation-overview.tsx
+++ src/components/donation-overview/donation-overview.tsx
@@ -2,13 +2,6 @@ import * as React from 'react';
 
 export default function DonationOverview(): React.ReactNode {
     return (
-        <div className="app" style={{height: '1500px'}}>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-            dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-            clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
-            consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-            sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no
-            sea takimata sanctus est Lorem ipsum dolor sit amet.
-        </div>
+        <div />
     );
 }
--- src/components/tab-bar/tab-bar.tsx
+++ src/components/tab-bar/tab-bar.tsx
@@ -1,52 +1,16 @@
 import * as React from 'react';
+import {useEffect} from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import {ReactComponent as Logo} from '../../assets/donation.svg';
 import './tab-bar.scss';
-import DonationOverview from '../donation-overview/donation-overview';
-import {Box} from '@material-ui/core';
-import Typography from '@material-ui/core/Typography';
 import SwipeableViews from 'react-swipeable-views';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import Slide from '@material-ui/core/Slide';
-import Toolbar from '@material-ui/core/Toolbar';
-
-interface TabPanelProps {
-    children?: React.ReactNode;
-    dir?: string;
-    index: any;
-    value: any;
-}
-
-function TabPanel(props: TabPanelProps) {
-    const {children, value, index, ...other} = props;
-
-    return (
-        <Typography
-            component="div"
-            role="tabpanel"
-            hidden={value !== index}
-            id={`full-width-tabpanel-${index}`}
-            aria-labelledby={`full-width-tab-${index}`}
-            {...other}
-        >
-            <Box p={3}>{children}</Box>
-        </Typography>
-    );
-}
-
-function a11yProps(index: any) {
-    return {
-        id: `full-width-tab-${index}`,
-        'aria-controls': `full-width-tabpanel-${index}`,
-    };
-}
+import {Box, Toolbar} from '@material-ui/core';
 
 export default function TabBar() {
     const [value, setValue] = React.useState(0);
-    const trigger = useScrollTrigger();
-
+    const [yOffset, setNavState] = React.useState(0);
     function handleChange(event: React.ChangeEvent<{}>, newValue: number) {
         setValue(newValue);
     }
@@ -55,43 +19,71 @@ export default function TabBar() {
         setValue(index);
     }
 
+    function transitionY() {
+        const transitionYthreshold = 48;
+        return Math.min(transitionYthreshold, yOffset);
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll, {passive: true});
+        return () => window.removeEventListener('scroll', handleScroll);
+    });
+
+    function handleScroll() {
+        setNavState(window.pageYOffset);
+    }
+
     return (
         <React.Fragment>
-                <AppBar position="fixed" color="default">
-                    <Slide appear={false} direction="down" in={!trigger}>
-                        <div className="tab-nav" style={{padding: '.5em 1em 0 1em'}}>
-                            <Logo style={{width: '30px', paddingRight: '1em'}}/>
-                            <span style={{fontWeight: 'bold', fontSize: '20px', verticalAlign: 'super'}}>Help-Educate</span>
-                        </div>
-                    </Slide>
-                    <Tabs
-                        value={value}
-                        onChange={handleChange}
-                        indicatorColor="primary"
-                        textColor="primary"
-                        variant="fullWidth"
-                    >
-                        <Tab label="Item One" {...a11yProps(0)}/>
-                        <Tab label="Item Two" {...a11yProps(1)}/>
-                        <Tab label="Item Three" {...a11yProps(2)}/>
-                    </Tabs>
-                </AppBar>
-            <Toolbar/>
+            <AppBar
+                position="sticky"
+                color="default"
+                style={{
+                    transition: 'all 0.1s',
+                    transform: `translateY(-${transitionY()}px)`
+                }}
+            >
+                <Toolbar style={{minHeight: '48px'}}>
+                    <Logo style={{width: '30px', marginRight: '1em'}}/>
+                    <span style={{fontWeight: 'bold', fontSize: '20px', verticalAlign: 'super'}}>Help-Educate</span>
+                </Toolbar>
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    indicatorColor="primary"
+                    textColor="primary"
+                    variant="fullWidth"
+                >
+                    <Tab label="Home"/>
+                    <Tab label="Live Donations"/>
+                    <Tab label="About Us"/>
+                </Tabs>
+            </AppBar>
             <SwipeableViews
                 index={value}
                 style={{height: '100%', width: '100%'}}
                 containerStyle={{height: '100%', width: '100%'}}
                 onChangeIndex={handleChangeIndex}
+                enableMouseEvents={true}
             >
-                <TabPanel value={value} index={0}>
-                    <DonationOverview/>
-                </TabPanel>
-                <TabPanel value={value} index={1}>
+                <Box>
+                    {
+                        [...new Array(20)].map(() =>
+                            `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+            dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+            clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+            consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+            sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no
+            sea takimata sanctus est Lorem ipsum dolor sit amet.`)
+                            .join('\n')
+                    }
+                </Box>
+                <div>
                     Item Two
-                </TabPanel>
-                <TabPanel value={value} index={2}>
+                </div>
+                <div>
                     Item Three
-                </TabPanel>
+                </div>
             </SwipeableViews>
         </React.Fragment>
     );
