fd9eac09b60208ed2028464e1f80f01c7e26a087
Marco <l.__marco@hotmail.de>
2019-11-01 09:17:27 +0100

    markers are now loaded into map, approaching to set the bounds into map

--- src/components/donations-map/map-overlay.jsx
+++ src/components/donations-map/map-overlay.jsx
@@ -1,35 +1,60 @@
-import React from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import 'leaflet/dist/leaflet.css';
 import './map-overlay.scss';
 import * as ReactLeaflet from 'react-leaflet';
-import { setSwipeState } from "../../store/actions";
-import { connect } from "react-redux";
+import {setSwipeState} from "../../store/actions";
+import {connect} from "react-redux";
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
 const {Map, TileLayer, Marker, Popup} = ReactLeaflet;
 
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
 function MapOverlay({setSwipeState}) {
-    const position = [51.505, -0.09];
     const zoom = 4;
+
+    const ref = useRef(null);
+    // const [bounds, setBounds] = useState([0, 0]);
+
+    useEffect(() => {
+        console.log(ref)
+        console.log(ref.current.leafletElement.getBounds())
+
+    }, []);
+
     return (
         <Map
-            center={position}
+            ref={ref}
+            center={[45.000, 10.000]}
             zoom={zoom}
             maxZoom={10}
             minZoom={2}
+            // bounds={ref.current.leafletElement.getBounds()}
             onMovestart={() => setSwipeState(false)}
             onMoveend={() => setSwipeState(true)}
-            maxBoundsViscosity={1.0}
         >
             <TileLayer
-              noWrap={true}
-              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                noWrap={true}
+                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={position}>
-                <Popup>
-                    A pretty CSS3 popup. <br/> Easily customizable.
-                </Popup>
-            </Marker>
+            {
+                [[51.505, -0.09], [58.403, 20.420], [43.300, 40]].map((position, i) =>
+
+                    <Marker position={position} key={i}>
+                        <Popup>
+                            A pretty CSS3 popup. <br/> Easily customizable.
+                        </Popup>
+                    </Marker>
+                )
+            }
         </Map>
     )
 }
