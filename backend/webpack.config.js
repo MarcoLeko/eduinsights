3da57faaf3772b8f5d3432ed094de7b09035ca13
Marco <l.__marco@hotmail.de>
2019-11-03 18:01:54 +0100

    Setup backend with express inversify and webpack

---
+++ backend/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+const terserOptions = {
+    parallel: true,
+    cache: true,
+    sourceMap: true,
+    terserOptions: {
+        compress: {
+            dead_code: true,
+            drop_console: true
+        },
+        output: {
+            comments: false
+        },
+    },
+};
+
+module.exports = {
+    entry: './src/index.ts',
+    mode: 'production',
+    devtool: 'source-map',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.js',
+    },
+    resolve: {
+        // Add `.ts` as a resolvable extension.
+        extensions: [".ts", ".js"]
+    },
+    target: "node",
+    externals: [nodeExternals()],
+    module: {
+        rules: [
+            // all files with a `.ts` extension will be handled by `ts-loader`
+            {
+                test: /\.ts?$/,
+                loader: "ts-loader",
+                exclude: /node_modules/
+            }
+        ]
+    },
+    optimization: {
+        minimizer: [
+            new TerserPlugin(
+                terserOptions
+            )],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+    ]
+};
