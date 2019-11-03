faa7f71555802451a939a7a0c7320e4b7a98fa55
Marco <l.__marco@hotmail.de>
2019-11-03 18:32:20 +0100

    setup express class as root dependency with inversify -setup done!

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,7 @@
-console.log('Server Started!');
+import depencyContainer from './di-config/inversify.config';
+import Express from './express';
+import {TYPES} from './di-config/types';
+
+const app = depencyContainer.get<Express>(TYPES.EXPRESS);
+
+app.start();
