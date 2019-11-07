a98a79642c48f0f1691867b8efeb9f89d9e14457
Marco <l.__marco@hotmail.de>
2019-11-07 12:10:16 +0100

    setup socket client for frontend

--- web/src/components/app/app.jsx
+++ web/src/components/app/app.jsx
@@ -5,13 +5,14 @@ import theme from '../../material-ui-theme';
 import {ThemeProvider} from '@material-ui/core/styles';
 import store from '../../store/store';
 import { Provider } from "react-redux";
+import socket from "../../socket-io-client";
 
 export default function App() {
 
     return (
         <ThemeProvider theme={theme}>
             <Provider store={store}>
-                <TabBar/>
+                <TabBar socket={socket}/>
             </Provider>
         </ThemeProvider>
     );
