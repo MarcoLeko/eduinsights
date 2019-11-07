f60203bd34e192f69340625ebe540e3c93af0ac8
Marco <l.__marco@hotmail.de>
2019-11-07 16:17:53 +0100

    setup for making collection queries but currently not working due to permissions-cap

--- backend/src/di-config/inversify.config.ts
+++ backend/src/di-config/inversify.config.ts
@@ -2,10 +2,12 @@ import {Container} from 'inversify';
 import {TYPES} from './types';
 import Express from '../modules/server/express';
 import SocketServer from '../modules/server/socket-server';
+import MongoDBClient from '../modules/db/mongo-db-client';
 
 const dependencyContainer = new Container();
 
 dependencyContainer.bind(TYPES.EXPRESS).to(Express);
 dependencyContainer.bind(TYPES.SOCKET_SERVER).to(SocketServer);
+dependencyContainer.bind(TYPES.MONGO_DB_CLIENT).to(MongoDBClient);
 
 export default dependencyContainer;
--- backend/src/di-config/types.ts
+++ backend/src/di-config/types.ts
@@ -1,4 +1,5 @@
 export const TYPES = {
     EXPRESS: Symbol.for('EXPRESS'),
-    SOCKET_SERVER: Symbol.for('SOCKET_SERVER')
+    SOCKET_SERVER: Symbol.for('SOCKET_SERVER'),
+    MONGO_DB_CLIENT: Symbol.for('MONGO_DB_CLIENT')
 };
