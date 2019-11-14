f23f42f68f9472219ae46f616a5f6dc77e6f7ba6
Marco Leko <Marco.Leko@partner.bmw.de>
2019-11-14 09:01:52 -0800

    code-cleanup for script generating statistics

--- tools/scripts/map-statistics/map-statistics-generator.js
+++ tools/scripts/map-statistics/map-statistics-generator.js
@@ -7,18 +7,20 @@ const HttpsProxyAgent = require('https-proxy-agent');
 
 (async function () {
     const unsecoStatisticsJSON = JSON.parse(fs.readFileSync(path.join(__dirname, 'proportion-primary-school-access-internet-2018.json'), 'utf8')),
-        contriesGeoJSON = JSON.parse(fs.readFileSync(path.join(__dirname, 'countries.geojson'), 'utf8')),
+        countriesGeoJSON = JSON.parse(fs.readFileSync(path.join(__dirname, 'countries.geojson'), 'utf8')),
         unsecoRegionsJSON = JSON.parse(fs.readFileSync(path.join(__dirname, 'regions.json'), 'utf8')),
+        unsecoHierarchicalCodeList = JSON.parse(fs.readFileSync(path.join(__dirname, 'hierarchical-codelist.json'), 'utf8')),
         availableCountriesStatistics = unsecoStatisticsJSON.structure.dimensions.series.find(data => data.id === "REF_AREA"),
-        finalMappedCountries = [],
+        resApiFromCountryCodeToName = 'https://restcountries.eu/rest/v2/alpha?codes=',
         notMatchingCountries = [],
         unescoRegions = new Map(),
+        finalMappedCountries = [],
         log = console.log;
 
 
     availableCountriesStatistics.values.forEach(statisticsCountries =>
 
-        contriesGeoJSON.features.forEach(geoJSONCountries => {
+        countriesGeoJSON.features.forEach(geoJSONCountries => {
             if (geoJSONCountries.properties.ADMIN === statisticsCountries.name) {
                 finalMappedCountries.push(statisticsCountries.name);
                 log(`Found matched contries within geoJSON/statistics sheet: ${chalk.green(geoJSONCountries.properties.ADMIN)}`);
@@ -40,28 +42,24 @@ const HttpsProxyAgent = require('https-proxy-agent');
 
     });
 
-    const url = 'https://restcountries.eu/rest/v2/alpha?codes=';
-    const codeList = JSON.parse(fs.readFileSync(path.join(__dirname, 'hierarchical-codelist.json'), 'utf8'));
-
     for (const r of unescoRegions.keys()) {
-        for (const list of codeList.HierarchicalCodelist) {
-        for (const entity of list.hierarchies) {
+        for (const list of unsecoHierarchicalCodeList.HierarchicalCodelist) {
+            for (const entity of list.hierarchies) {
+                if (r === entity.id) {
+                    for (const sub of entity.codes[0].codes) {
+                        try {
+                            const response = await fetch(resApiFromCountryCodeToName + sub.id, {agent: new HttpsProxyAgent('http://localhost:3128')});
+                            const result = await response.json();
+                            log(`Id: ${chalk.yellow.bold(sub.id)} Name: ${chalk.magenta(result[0].name)}`);
+                            unescoRegions.get(r).push(result[0].name);
+                        } catch (e) {
+                            log(`Could not fetch country name for id: ${chalk.red.bold(sub.id)} ` + e);
 
-            if (r === entity.id) {
-                for (const sub of entity.codes[0].codes) {
-                    try {
-                        const response = await fetch('https://restcountries.eu/rest/v2/alpha?codes=' + sub.id, {agent: new HttpsProxyAgent('http://localhost:3128')});
-                        const result = await response.json();
-                        log(`id: ${chalk.yellow.bold(sub.id)} name: ${chalk.magenta(result[0].name)}`);
-                        unescoRegions.get(r).push(result[0].name);
-                    } catch (e) {
-                        log(`Could not fetch country name for id: ${chalk.red.bold(sub.id)} ` + e);
-
+                        }
                     }
                 }
             }
         }
-    }
     }
 
     console.log(unescoRegions)
