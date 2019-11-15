b38f8f2f902a8362453dc79c0b44714b7527d3e4
Marco Leko <Marco.Leko@partner.bmw.de>
2019-11-15 03:20:21 -0800

    Finally made a script for presenting statistics on the map overlay

--- tools/scripts/map-statistics/map-statistics-generator.js
+++ tools/scripts/map-statistics/map-statistics-generator.js
@@ -9,12 +9,12 @@ const HttpsProxyAgent = require('https-proxy-agent');
     const unsecoStatisticsJSON = JSON.parse(fs.readFileSync(path.join(__dirname, 'proportion-primary-school-access-internet-2018.json'), 'utf8')),
         countriesGeoJSON = JSON.parse(fs.readFileSync(path.join(__dirname, 'countries.geojson'), 'utf8')),
         unsecoRegionsJSON = JSON.parse(fs.readFileSync(path.join(__dirname, 'regions.json'), 'utf8')),
-        unsecoHierarchicalCodeList = JSON.parse(fs.readFileSync(path.join(__dirname, 'hierarchical-codelist.json'), 'utf8')),
+        unsecoHierarchicalCodeListJSON = JSON.parse(fs.readFileSync(path.join(__dirname, 'hierarchical-codelist.json'), 'utf8')),
         availableCountriesStatistics = unsecoStatisticsJSON.structure.dimensions.series.find(data => data.id === "REF_AREA"),
         resApiFromCountryCodeToName = 'https://restcountries.eu/rest/v2/alpha?codes=',
         notMatchingCountries = [],
         unescoRegions = new Map(),
-        finalMappedCountries = [],
+        unescoCountries = [],
         log = console.log;
 
 
@@ -22,7 +22,7 @@ const HttpsProxyAgent = require('https-proxy-agent');
 
         countriesGeoJSON.features.forEach(geoJSONCountries => {
             if (geoJSONCountries.properties.ADMIN === statisticsCountries.name) {
-                finalMappedCountries.push(statisticsCountries.name);
+                unescoCountries.push(statisticsCountries.name);
                 log(`Found matched contries within geoJSON/statistics sheet: ${chalk.green(geoJSONCountries.properties.ADMIN)}`);
             } else {
                 if (!notMatchingCountries.includes(statisticsCountries.name)) {
@@ -32,7 +32,9 @@ const HttpsProxyAgent = require('https-proxy-agent');
         })
     );
 
+
     notMatchingCountries.forEach(c => {
+
         log(`Could not found values matched contries within geoJSON/statistics sheet: ${chalk.red.bold(c)}`);
         unsecoRegionsJSON.Codelist[0].items.forEach(unR => {
             if (c === unR.names[0].value && /^[a-z]+_[a-z]+$/i.test(unR.id)) {
@@ -42,9 +44,13 @@ const HttpsProxyAgent = require('https-proxy-agent');
 
     });
 
+
     for (const r of unescoRegions.keys()) {
-        for (const list of unsecoHierarchicalCodeList.HierarchicalCodelist) {
+
+        for (const list of unsecoHierarchicalCodeListJSON.HierarchicalCodelist) {
+
             for (const entity of list.hierarchies) {
+
                 if (r === entity.id) {
                     for (const sub of entity.codes[0].codes) {
                         try {
@@ -57,12 +63,63 @@ const HttpsProxyAgent = require('https-proxy-agent');
 
                         }
                     }
+
+                }
+
+            }
+
+        }
+    }
+
+    function getUnsecoStatisticsEntityByIndex(i) {
+        return unsecoStatisticsJSON.dataSets[0].series[`0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:${i}`].observations['0'][0];
+    }
+
+    const resultArrayWithCountryMatches = unescoCountries.reduce((acc, curr, i) => {
+        for (const geo of countriesGeoJSON.features) {
+            if (geo.properties.ADMIN === curr) {
+                const value = getUnsecoStatisticsEntityByIndex(i);
+                const geoJSONIndex = countriesGeoJSON.features.findIndex(obj => obj.properties.ADMIN === curr);
+                const {properties} = countriesGeoJSON.features[geoJSONIndex];
+                delete properties.ADMIN;
+                delete properties["ISO_A3"];
+                Object.assign(properties, {name: curr, value});
+                acc.push(countriesGeoJSON.features[geoJSONIndex]);
+                return acc;
+            }
+        }
+    }, []);
+
+    const resultArrayWithRegionMatches = [];
+
+    for (const [key, value] of unescoRegions.entries()) {
+        const statisticIndex = availableCountriesStatistics.values.findIndex(obj => obj.id === key);
+
+        for (const c of value) {
+            for (const geo of countriesGeoJSON.features) {
+                if (geo.properties.ADMIN === c) {
+                    const value = getUnsecoStatisticsEntityByIndex(statisticIndex);
+                    const geoJSONIndex = countriesGeoJSON.features.findIndex(obj => obj.properties.ADMIN === c);
+                    const {properties} = countriesGeoJSON.features[geoJSONIndex];
+                    delete properties.ADMIN;
+                    delete properties["ISO_A3"];
+                    Object.assign(properties, {name: c, value});
+                    resultArrayWithRegionMatches.push(countriesGeoJSON.features[geoJSONIndex]);
                 }
             }
         }
     }
 
-    console.log(unescoRegions)
+    const output = {
+        "type": "FeatureCollection",
+        "features": resultArrayWithCountryMatches.concat(resultArrayWithRegionMatches)
+    };
+
+    if (!fs.existsSync(path.join(__dirname, 'output'))) {
+        fs.mkdirSync(path.join(__dirname, 'output'));
+    }
+
+    fs.writeFileSync(path.join(__dirname, 'output', 'output.json'), JSON.stringify(output))
 })();
 
 
