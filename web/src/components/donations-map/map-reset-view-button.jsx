7e987b6f62620798c0f02c092c1d393c342d953e
Marco Leko <Marco.Leko@partner.bmw.de>
2019-11-18 08:48:55 -0800

    added reset view button on map

---
+++ web/src/components/donations-map/map-reset-view-button.jsx
@@ -0,0 +1,32 @@
+import {useLeaflet} from "react-leaflet";
+import React, {useEffect} from 'react';
+import L from "leaflet";
+
+/**
+ * @return {null}
+ */
+function MapResetViewButton() {
+
+    const {map} = useLeaflet();
+    const button = L.control({position: "topleft"});
+
+    useEffect(() => {
+
+        button.onAdd = () => {
+            const div = L.DomUtil.create("button", "button ripple MuiButton-root MuiButton-contained");
+            div.innerHTML = 'Reset View';
+
+            L.DomEvent.disableClickPropagation(div)
+                .addListener(div, 'click', function () {
+                    map.setView(map.options.center, map.options.zoom)
+                }, div);
+            return div;
+        };
+
+        button.addTo(map);
+    }, []);
+
+    return null;
+}
+
+export default MapResetViewButton;
