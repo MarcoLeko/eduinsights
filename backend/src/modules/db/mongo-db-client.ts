1b45f4e5fb0aefab18cfcf260dd9546bfed937f2
Marco <l.__marco@hotmail.de>
2019-12-11 09:42:19 +0100

    now validateSession works fine

--- backend/src/modules/db/mongo-db-client.ts
+++ backend/src/modules/db/mongo-db-client.ts
@@ -8,19 +8,17 @@ export default class MongoDBClient {
     private static readonly userName: string = process.env.DB_USERNAME as string;
     private static readonly password: string = process.env.DB_PASSWORD as string;
     private static readonly URI: string = `mongodb+srv://${MongoDBClient.userName}:${MongoDBClient.password}@help-educate-vj2pu.mongodb.net?retryWrites=true&w=majority`;
-
+    public mongoClient: MongoClient = new MongoClient(MongoDBClient.URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    } as MongoClientOptions);
     private connectionManager: MongoClient;
-    public mongoClient: MongoClient = new MongoClient(MongoDBClient.URI, {useNewUrlParser: true, useUnifiedTopology: true} as MongoClientOptions);
 
-    public connect(): Promise<void| string> {
+    public connect(): Promise<void | string> {
         return this.mongoClient.connect()
             .then((mongoClient: MongoClient) => this.connectionManager = mongoClient)
             .then(() => this.setupDBIndexes())
             .catch((e: Error) => console.log('Could not connect to mongo server: ' + e));
-    }
-
-    private setupDBIndexes() {
-        return this.connectionManager.db('users').collection('email').createIndex( {"email": 1} as Object, {unique: true})
     }
 
     public getCollectionOfCharities() {
@@ -46,26 +44,37 @@ export default class MongoDBClient {
         }
     }
 
-    public async addUser({ firstName, lastName, avatarColor, email, password, persistLogin } : User) {
+    public async addUser({firstName, lastName, avatarColor, email, password, persistLogin}: User) {
         const passwordHashed: string = await CredentialHelper.hash(password);
         return this.connectionManager.db('users').collection<User>('email').insertOne(
-        { firstName, lastName, avatarColor, email, persistLogin, password: passwordHashed }
-        )
+            {firstName, lastName, avatarColor, email, persistLogin, password: passwordHashed}
+        );
     }
 
     public async findUserByEmail(email: string) {
-        return this.connectionManager.db('users').collection<User>('email').findOne({email})
+        return this.connectionManager.db('users').collection<User>('email').findOne({email});
     }
 
     public async updateUser(email: string, toUpdate: Partial<User>): Promise<UpdateWriteOpResult> {
-        return this.connectionManager.db('users').collection<User>('email').updateOne({email: email}, {"$set": toUpdate})
-    }
-    public async findUserByID(id: string) {
-        return this.connectionManager.db('users').collection<User>('email').findOne({"_id": new ObjectId(id)} as any)
+        return this.connectionManager.db('users').collection<User>('email').updateOne({email: email}, {'$set': toUpdate});
     }
 
-    public async compareSessionIds(uid: string, sid: string) {
-        const user = await this.connectionManager.db('users').collection<User>('sessions').findOne({uid});
-        return user && user._id === sid;
+    public async findUserByID(id: string) {
+        return this.connectionManager.db('users').collection<User>('email').findOne({'_id': new ObjectId(id)} as any);
+    }
+
+    public async validatedSession(uid: string, sid: string) {
+        try {
+            const user: any = await this.connectionManager.db('users').collection<User>('sessions').findOne({"session": /uid/});
+            return user && sid.replace('s:', '').includes(user._id);
+        } catch (e) {
+            console.log(e);
+            return false;
+        }
+
+    }
+
+    private setupDBIndexes() {
+        return this.connectionManager.db('users').collection('email').createIndex({'email': 1} as Object, {unique: true});
     }
 }
