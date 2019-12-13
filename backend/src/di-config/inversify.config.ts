9089203634a2ff09e088235677886696d45a2a34
Marco Leko <Marco.Leko@partner.bmw.de>
2019-12-13 01:51:00 -0800

    user receives now email after regististration process

--- backend/src/di-config/inversify.config.ts
+++ backend/src/di-config/inversify.config.ts
@@ -3,11 +3,13 @@ import {TYPES} from './types';
 import Express from '../modules/server/express';
 import MongoDBClient from '../modules/db/mongo-db-client';
 import CredentialHelper from "../modules/db/credential-helper";
+import EmailCreator from "../modules/email-manager/email-creator";
 
 const dependencyContainer = new Container();
 
 dependencyContainer.bind(TYPES.EXPRESS).to(Express);
 dependencyContainer.bind(TYPES.MONGO_DB_CLIENT).to(MongoDBClient);
 dependencyContainer.bind(TYPES.HASH_GENERATOR).to(CredentialHelper);
+dependencyContainer.bind(TYPES.EMAIL_CREATOR).to(EmailCreator);
 
 export default dependencyContainer;
