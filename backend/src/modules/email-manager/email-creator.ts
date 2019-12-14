f5053fd883bd8272241875db615e2249da131b4a
Marco <l.__marco@hotmail.de>
2019-12-14 15:49:14 +0100

    sending transactional emails and creating verification token works now on backend

--- backend/src/modules/email-manager/email-creator.ts
+++ backend/src/modules/email-manager/email-creator.ts
@@ -1,50 +1,38 @@
-import {inject, injectable} from 'inversify';
-import handlebars from 'handlebars';
-import fs from 'fs';
-import {User, UserToken} from "../../types/types";
-import path from 'path';
-import {joinDir} from "../utils/paths";
+import {injectable} from 'inversify';
 import sendgridMail from '@sendgrid/mail';
 import {MailData} from '@sendgrid/helpers/classes/mail';
-import {TYPES} from "../../di-config/types";
-import MongoDBClient from "../db/mongo-db-client";
 
 @injectable()
 export default class EmailCreator {
 
     private static EMAIL_FROM = 'no-reply@help-educate.com';
+    private static TEMPLATE_ID_EMAIL_VERIFY = 'd-8ca7682e287d47428c351e7854d98567';
 
-    constructor(
-        @inject(TYPES.MONGO_DB_CLIENT) private mongoDBClient: MongoDBClient
-    ) {
+    constructor() {
         sendgridMail.setApiKey(process.env.SEND_GRID_API_KEY as string);
     }
 
-    private static transformTemplate(data: Object, path: string): string {
-        const html = handlebars.compile(fs.readFileSync(path, 'utf-8'));
-        return html(data);
-    }
-
     public async sendEmailVerificationLink(data: any) {
-        const {firstName, email, uid} = data;
+        const {firstName, email} = data;
         const token = Math.random().toString(36).substr(2);
-
-        console.log(token);
-
         const message: MailData = {
-            to: email,
             from: EmailCreator.EMAIL_FROM,
-            subject: 'Help-educate Support',
-            html: EmailCreator.transformTemplate({
-                firstName,
-                link: `http://localhost:8080/verify-email?token=${token}`
-            }, path.join(joinDir('src/modules/email-manager/templates/email-verification.html')))
+            personalizations: [{
+                to: [{
+                    email,
+                }],
+                dynamicTemplateData: {
+                    firstName,
+                    'verificationLink': `http://localhost:4200/verify-email?token=${token}`
+                },
+            }],
+            templateId: EmailCreator.TEMPLATE_ID_EMAIL_VERIFY
         };
 
         await sendgridMail.send(message);
 
         // expires in 7 days from now
         const expireAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
-        return this.mongoDBClient.addEmailVerificationToken({uid, token, expireAt} as UserToken);
+        return {expireAt, token};
     }
 }
