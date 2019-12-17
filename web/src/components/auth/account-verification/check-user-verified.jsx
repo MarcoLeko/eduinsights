85291af1f428737081e60e932e6d0cdd063f9c9f
Marco Leko <Marco.Leko@partner.bmw.de>
2019-12-17 08:20:38 -0800

    extended ui for email-validation view

--- web/src/components/auth/account-verification/check-user-verified.jsx
+++ web/src/components/auth/account-verification/check-user-verified.jsx
@@ -1,22 +1,33 @@
-import React, {useEffect} from "react";
-import {checkEmailToken} from "../../../store/thunks";
-import {connect} from "react-redux";
+import React, {useEffect, useState} from "react";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Button from "@material-ui/core/Button";
 
-function CheckUserVerified({firstName, email, token}) {
+function CheckUserVerified({token}) {
+
+    const [verified, setVerified] = useState(false);
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
-        checkEmailToken({token})
+        setTimeout(() => {
+            setLoading(false);
+            setTimeout(() => setVerified(true), 5000);
+        }, 5000)
+        // checkEmailToken({token})
     }, [token]);
+
     return (
-        <>
-            <h1>Hello {firstName}</h1>
-            <div>{email} is verified with token: {token}</div>
-        </>
+        <Grid container justify={"center"} alignItems={"center"} direction={"column"}>
+            {loading ? <CircularProgress color={"secondary"}/> :
+                verified ? <Typography color="primary" variant={"h6"}>Your account is now verified.</Typography>
+                    : <>
+                        <Typography color="primary" variant={"h6"}>Your token is expired.</Typography>
+                        <Button variant="contained" color="secondary">Verify Email</Button>
+                    </>
+            }
+        </Grid>
+
     )
 }
 
-const mapStateToProps = store => ({
-    firstName: store.authReducer.firstName,
-    email: store.authReducer.email,
-});
-
-export default connect(mapStateToProps)(CheckUserVerified)
+export default CheckUserVerified
