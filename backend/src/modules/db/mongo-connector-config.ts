c1b2a611b6f4101f46cfbdfb92321c38747ea5aa
Marco Leko <Marco.Leko@partner.bmw.de>
2019-12-17 06:03:46 -0800

    added tab icons for larger screens

--- backend/src/modules/db/mongo-connector-config.ts
+++ backend/src/modules/db/mongo-connector-config.ts
@@ -3,7 +3,8 @@ import {MongoClient, MongoClientOptions} from "mongodb";
 export default function connectorInstance(): MongoClient {
     const userName: string = process.env.DB_USERNAME as string;
     const password: string = process.env.DB_PASSWORD as string;
-    const URI: string = `mongodb+srv://${userName}:${password}@help-educate-vj2pu.mongodb.net?retryWrites=true&w=majority`;
+    const URI: string = `mongodb://127.0.0.1:27017`;
+    // const URI: string = `mongodb+srv://${userName}:${password}@help-educate-vj2pu.mongodb.net?retryWrites=true&w=majority`;
     return new MongoClient(URI, {
         useNewUrlParser:    true,
         useUnifiedTopology: true
