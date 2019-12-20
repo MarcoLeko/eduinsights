c04a3c17875bf659f4d4079438a82cf0080a6d52
Marco <l.__marco@hotmail.de>
2019-12-20 11:32:54 +0100

    adopted ui for verified user

--- web/src/components/shared/user-avatar.jsx
+++ web/src/components/shared/user-avatar.jsx
@@ -1,50 +1,60 @@
-import Badge from "@material-ui/core/Badge";
+import Badge from '@material-ui/core/Badge';
 import React from 'react';
-import Avatar from "@material-ui/core/Avatar";
-import withStyles from "@material-ui/core/styles/withStyles";
-import {connect} from "react-redux";
+import Avatar from '@material-ui/core/Avatar';
+import withStyles from '@material-ui/core/styles/withStyles';
+import {connect} from 'react-redux';
+import clsx from 'clsx';
+import makeStyles from '@material-ui/core/styles/makeStyles';
 
 const StyledBadge = withStyles(theme => ({
-    badge: {
-        color: '#00b4ff',
-        width: 16,
-        height: 16,
-        backgroundColor: theme.palette.background.paper,
-        fontSize: 14,
-        bottom: '10%',
-        '&::after': {
-            position: 'absolute',
-            content: '"\u2714"',
-        },
+  badge: {
+    backgroundColor: '#44b700',
+    color: '#44b700',
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    '&::after': {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      borderRadius: '50%',
+      content: '""',
     },
+  },
 }))(Badge);
 
-
 function VerifiedBadge({children}) {
-    return (
-        <StyledBadge
-            overlap="circle"
-            anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'right',
-            }}
-            variant="dot"
-        >{children}</StyledBadge>
-    );
+  return (
+      <StyledBadge
+          overlap="circle"
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+          }}
+          variant="dot"
+      >{children}</StyledBadge>
+  );
 }
 
+const useStyles = makeStyles(theme => ({
+    verified: {
+      boxShadow: `0 0 0 2px ${theme.palette.primary.main}`,
+    }
+}));
 function UserAvatar(props) {
-    const {verified, style, children} = props;
-    return (
-        verified ?
-            <VerifiedBadge>
-                <Avatar style={style}>{children}</Avatar>
-            </VerifiedBadge> :
-            <Avatar style={style}>{children}</Avatar>
-    )
+  const classes = useStyles();
+
+  const {verified, style, children} = props;
+  return (
+      verified ?
+          <VerifiedBadge>
+            <Avatar className={clsx(verified && classes.verified)}style={style}>{children}</Avatar>
+          </VerifiedBadge> :
+          <Avatar style={style}>{children}</Avatar>
+  );
 }
 
 const mapStateToProps = store => ({
-    verified: store.authReducer.emailVerified
+  verified: store.authReducer.emailVerified,
 });
 export default connect(mapStateToProps)(UserAvatar);
