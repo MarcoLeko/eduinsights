25c6ec00c26aa4068eb56fa1fb56759e220b101a
Marco <l.__marco@hotmail.de>
2019-12-21 17:57:24 +0100

    added package.json to bundle as it is needed to start the app in a vm

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -5,53 +5,60 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 const terserOptions = {
-    parallel: true,
-    cache: true,
-    sourceMap: true,
-    terserOptions: {
-        compress: {
-            dead_code: true
-        },
-        output: {
-            comments: false
-        },
+  parallel: true,
+  cache: true,
+  sourceMap: true,
+  terserOptions: {
+    compress: {
+      dead_code: true,
     },
+    output: {
+      comments: false,
+    },
+  },
 };
 
 module.exports = {
-    entry: './src/index.ts',
-    mode: 'production',
-    devtool: 'source-map',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'backend/app.js',
-    },
-    resolve: {
-        // Add `.ts` as a resolvable extension.
-        extensions: [".ts", ".js"]
-    },
-    target: "node",
-    externals: [nodeExternals()],
-    module: {
-        rules: [
-            // all files with a `.ts` extension will be handled by `ts-loader`
-            {
-                test: /\.ts?$/,
-                loader: "ts-loader",
-                exclude: /node_modules/
-            }
-        ]
-    },
-    optimization: {
-        minimizer: [
-            new TerserPlugin(
-                terserOptions
-            )],
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new CopyPlugin([
-            { from: '../web/build', to: 'web/build' }
-        ]),
-    ]
+  entry: './src/index.ts',
+  mode: 'production',
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'backend/app.js',
+  },
+  resolve: {
+    // Add `.ts` as a resolvable extension.
+    extensions: ['.ts', '.js'],
+  },
+  target: 'node',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      // all files with a `.ts` extension will be handled by `ts-loader`
+      {
+        test: /\.ts?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin(
+          terserOptions,
+      )],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin([
+      {from: '../web/build', to: 'web/build'},
+      {
+        from: './package.json', transform(content) {
+          const json = JSON.parse(content.toString());
+          json.main = 'backend/app.js';
+          return Buffer.from(JSON.stringify(json), 'utf8');
+        },
+      },
+    ]),
+  ],
 };
