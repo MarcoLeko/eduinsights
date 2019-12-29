425b6fcd23f48bceb22c82da350171f512940092
Marco <l.__marco@hotmail.de>
2019-12-29 17:04:02 +0100

    dev build is now referencing to correct static files path

--- backend/config/env.properties.ts
+++ backend/config/env.properties.ts
@@ -2,22 +2,25 @@ const CONFIG_PRODUCTION = {
     PATH_TO_STATIC_FILES: 'web/build',
     SECURE_COOKIE: true,
     PORT: process.env.PORT,
-    DB_USERNAME:process.env.DB_USERNAME,
-    DB_PASSWORD:process.env.DB_PASSWORD,
-    SESSION_SECRET:process.env.SESSION_SECRET,
-    SEND_GRID_API_KEY:process.env.SEND_GRID_API_KEY
+    DB_USERNAME: process.env.DB_USERNAME,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    SESSION_SECRET: process.env.SESSION_SECRET,
+    SEND_GRID_API_KEY: process.env.SEND_GRID_API_KEY
 };
 
 const CONFIG_DEVELOPMENT = {
-    PATH_TO_STATIC_FILES: '../web/build',
+    PATH_TO_STATIC_FILES: 'web/build',
     SECURE_COOKIE: true,
     PORT: process.env.PORT,
-    DB_USERNAME:process.env.DB_USERNAME,
-    DB_PASSWORD:process.env.DB_PASSWORD,
-    SESSION_SECRET:process.env.SESSION_SECRET,
-    SEND_GRID_API_KEY:process.env.SEND_GRID_API_KEY
+    DB_USERNAME: process.env.DB_USERNAME,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    SESSION_SECRET: process.env.SESSION_SECRET,
+    SEND_GRID_API_KEY: process.env.SEND_GRID_API_KEY
 };
 
-const CONFIG_LOCAL = Object.assign(CONFIG_DEVELOPMENT, {SECURE_COOKIE: false});
+const CONFIG_LOCAL = Object.assign({}, CONFIG_DEVELOPMENT, {
+    PATH_TO_STATIC_FILES: '../web/build',
+    SECURE_COOKIE: false
+});
 
 export {CONFIG_PRODUCTION, CONFIG_DEVELOPMENT, CONFIG_LOCAL};
