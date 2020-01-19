562e62361a98c0c480671f405269279a5b4928c0
Marco Leko <Marco.Leko@jsmd-group.com>
2020-01-19 16:26:28 +0100

    adopted file bundling script

--- tools/scripts/aws/pack-bundle.js
+++ tools/scripts/aws/pack-bundle.js
@@ -35,14 +35,7 @@ const archiver = require('archiver');
   // listen for all archive data to be written
   // 'close' event is fired only when a file descriptor is involved
   output.on('close', function() {
-    log((`${chalk.bold.green(formatBytes(archive.pointer()))} total compressed.`));
-  });
-
-  // This event is fired when the data source is drained no matter what was the data source.
-  // It is not part of this library but rather from the NodeJS Stream API.
-  // @see: https://nodejs.org/api/stream.html#stream_event_end
-  output.on('end', function() {
-    console.log('Data has been drained');
+    log((`${chalk.bold.green(formatBytes(archive.pointer()))} total compressed: ${chalk.bold.blue(path.resolve(__dirname, `${appName}.zip`))}`));
   });
 
   // good practice to catch warnings (ie stat failures and other non-blocking errors)
