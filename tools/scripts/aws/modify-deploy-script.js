fe6ba9a28b12b2fb2048085312cce94272febc3c
Marco <l.__marco@hotmail.de>
2020-01-25 13:21:16 +0100

    added deployment-script for coninious delivery

---
+++ tools/scripts/aws/modify-deploy-script.js
@@ -0,0 +1,16 @@
+'use strict';
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+
+(function() {
+  const log = console.log,
+      {name, version} = JSON.parse(fs.readFileSync(path.join(__dirname, '../../..', '/backend', 'package.json'))),
+      pathToConfigurationFile = path.join(__dirname, '../../..', '.elasticbeanstalk', 'config.yml'),
+      configurationFile = fs.readFileSync(pathToConfigurationFile).toString(),
+      appName = `${name}@${version}`;
+
+  log(`Applying app version ${chalk.bold.blue(appName)} for deployment.`);
+
+  fs.writeFileSync(pathToConfigurationFile, configurationFile.replace(/^.*artifact.*$/mg, `artifact: ${appName}`));
+})();
