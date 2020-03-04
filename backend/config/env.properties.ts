92bd64ddd156b28ee12dd5cced3afecac5bb45e1
Marco Leko <Marco.Leko@jsmd-group.com>
2020-03-04 16:59:40 +0100

    now using only local and prod env

--- backend/config/env.properties.ts
+++ backend/config/env.properties.ts
@@ -9,18 +9,7 @@ const CONFIG_PRODUCTION = {
     SEND_GRID_API_KEY: process.env.SEND_GRID_API_KEY
 };
 
-const CONFIG_DEVELOPMENT = {
-    env: process.env.NODE_ENV,
-    PATH_TO_STATIC_FILES: 'web/build',
-    SECURE_COOKIE: false,
-    PORT: process.env.PORT,
-    DB_USERNAME: process.env.DB_USERNAME,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    SESSION_SECRET: process.env.SESSION_SECRET,
-    SEND_GRID_API_KEY: process.env.SEND_GRID_API_KEY
-};
-
-const CONFIG_LOCAL = Object.assign(CONFIG_DEVELOPMENT, {
+const CONFIG_LOCAL = Object.assign(CONFIG_PRODUCTION, {
     env: process.env.NODE_ENV,
     PATH_TO_STATIC_FILES: '../web/build'
 });
--- backend/src/di-config/inversify.config.ts
+++ backend/src/di-config/inversify.config.ts
@@ -1,41 +1,50 @@
-import 'reflect-metadata';
-import {Container} from 'inversify';
-import {TYPES} from './types';
-import Express from '../modules/server/express';
-import MongoDBClient from '../modules/db/mongo-db-client';
-import CredentialHelper from '../modules/db/credential-helper';
-import EmailCreator from '../modules/email-manager/email-creator';
-import AuthRoutes from '../modules/server/routes/auth-routes';
-import ApiRoutes from '../modules/server/routes/api-routes';
-import AbstractRoutes from '../modules/server/routes/abstract-routes';
-import {MongoDBClientProvider} from '../types/types';
-import {Environment} from '../modules/utils/environment';
-import {CONFIG_DEVELOPMENT, CONFIG_LOCAL, CONFIG_PRODUCTION} from '../../config/env.properties';
+import "reflect-metadata";
+import { Container } from "inversify";
+import { TYPES } from "./types";
+import Express from "../modules/server/express";
+import MongoDBClient from "../modules/db/mongo-db-client";
+import CredentialHelper from "../modules/db/credential-helper";
+import EmailCreator from "../modules/email-manager/email-creator";
+import AuthRoutes from "../modules/server/routes/auth-routes";
+import ApiRoutes from "../modules/server/routes/api-routes";
+import AbstractRoutes from "../modules/server/routes/abstract-routes";
+import { MongoDBClientProvider } from "../types/types";
+import { Environment } from "../modules/utils/environment";
+import { CONFIG_DEVELOPMENT, CONFIG_LOCAL, CONFIG_PRODUCTION } from "../../config/env.properties";
 
 const dependencyContainer = new Container();
 
-dependencyContainer.bind<Express>(TYPES.EXPRESS).to(Express).inSingletonScope();
-dependencyContainer.bind<EmailCreator>(TYPES.EMAIL_CREATOR).to(EmailCreator).inSingletonScope();
-dependencyContainer.bind<AbstractRoutes>(TYPES.ABSTRACT_ROUTES).to(ApiRoutes).inTransientScope();
-dependencyContainer.bind<AbstractRoutes>(TYPES.ABSTRACT_ROUTES).to(AuthRoutes).inTransientScope();
-dependencyContainer.bind<MongoDBClient>(TYPES.MONGO_DB_CLIENT).to(MongoDBClient).inSingletonScope();
-dependencyContainer.bind<CredentialHelper>(TYPES.HASH_GENERATOR).to(CredentialHelper).inSingletonScope();
+dependencyContainer
+  .bind<Express>(TYPES.EXPRESS)
+  .to(Express)
+  .inSingletonScope();
+dependencyContainer
+  .bind<EmailCreator>(TYPES.EMAIL_CREATOR)
+  .to(EmailCreator)
+  .inSingletonScope();
+dependencyContainer
+  .bind<AbstractRoutes>(TYPES.ABSTRACT_ROUTES)
+  .to(ApiRoutes)
+  .inTransientScope();
+dependencyContainer
+  .bind<AbstractRoutes>(TYPES.ABSTRACT_ROUTES)
+  .to(AuthRoutes)
+  .inTransientScope();
+dependencyContainer
+  .bind<MongoDBClient>(TYPES.MONGO_DB_CLIENT)
+  .to(MongoDBClient)
+  .inSingletonScope();
+dependencyContainer
+  .bind<CredentialHelper>(TYPES.HASH_GENERATOR)
+  .to(CredentialHelper)
+  .inSingletonScope();
 
-dependencyContainer.bind<MongoDBClientProvider>(TYPES.MONGO_DB_CLIENT_PROVIDER)
-    .toProvider((context) => () => context.container.get<MongoDBClient>(TYPES.MONGO_DB_CLIENT).connect());
+dependencyContainer
+  .bind<MongoDBClientProvider>(TYPES.MONGO_DB_CLIENT_PROVIDER)
+  .toProvider(context => () => context.container.get<MongoDBClient>(TYPES.MONGO_DB_CLIENT).connect());
 
-dependencyContainer.bind<Object>(TYPES.ENVIRONMENTAL_CONFIG).toFactory(
-    () => (context: Environment) => {
-
-        switch (context) {
-            case Environment.development:
-                return CONFIG_DEVELOPMENT;
-            case Environment.production:
-                return CONFIG_PRODUCTION;
-            case Environment.local:
-            default:
-                return CONFIG_LOCAL;
-        }
-    });
+dependencyContainer
+  .bind<Object>(TYPES.ENVIRONMENTAL_CONFIG)
+  .toFactory(() => (context: Environment) => (context === Environment.production ? CONFIG_PRODUCTION : CONFIG_LOCAL));
 
 export default dependencyContainer;
