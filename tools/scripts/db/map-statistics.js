4138cd4ad85ba633cf8c20ee2f1a835082cf86b6
Marco Leko <marco.leko@jsmd-group.com>
2020-12-15 09:23:26 +0100

    provide multiple statistics from unesco within obj.

---
+++ tools/scripts/db/map-statistics.js
@@ -0,0 +1,25 @@
+module.exports = mapStatistics = {
+  // proportion of primary schools with access to internet for pedagogical purposes (%)
+  PROPORTION_OF_PRIMARY_SCHOOLS_INTERNET_ACCESS_2018_2018:
+    "https://api.uis.unesco.org/sdmx/data/SDG4/SCH.PT.L1._T._T._T._T.INST_T._Z._T._Z.NET_PP._T._T._T._Z._Z._Z.?startPeriod=2018&endPeriod=2018&format=sdmx-json&subscription-key=",
+  // gross enrolment rate for primary education in any sex and any country
+  GROSS_ENROLMENT_RATE_FOR_PRIMARY_EDUCATION_2018_2018:
+    "https://api.uis.unesco.org/sdmx/data/UNESCO,EDU_NON_FINANCE,3.0/GER.PT.L1.._T.._T.INST_T._Z._Z._T._T._T._Z._Z._Z._Z._Z.W00.W00._Z.?startPeriod=2018&endPeriod=2018&format=sdmx-json&locale=en&subscription-key=",
+  // Graduates percentage of bachelors or equivalent level
+  PERCENTAGE_OF_GRADUATES_BACHELORS_TOTAL_2018_2018:
+    "https://api.uis.unesco.org/sdmx/data/UNESCO,EDU_NON_FINANCE,3.0/GRAD.PT.L6...................?startPeriod=2018&endPeriod=2018&format=sdmx-json&locale=en&subscription-key=",
+  PERCENTAGE_OF_GRADUATES_BACHELORS_FEMALE_2018_2018:
+    "https://api.uis.unesco.org/sdmx/data/UNESCO,EDU_NON_FINANCE,3.0/FGP.PT.L6...................?startPeriod=2018&endPeriod=2018&format=sdmx-json&locale=en&subscription-key=",
+  // Graduates percentage of doctoral
+  PERCENTAGE_OF_GRADUATES_DOCTORAL_TOTAL_2018_2018:
+    "https://api.uis.unesco.org/sdmx/data/UNESCO,EDU_NON_FINANCE,3.0/GRAD.PT.L8...................?startPeriod=2018&endPeriod=2018&format=sdmx-json&locale=en&subscription-key=",
+  PERCENTAGE_OF_GRADUATES_DOCTORAL_FEMALE_2018_2018:
+    "https://api.uis.unesco.org/sdmx/data/UNESCO,EDU_NON_FINANCE,3.0/FGP.PT.L8...................?startPeriod=2018&endPeriod=2018&format=sdmx-json&locale=en&subscription-key=",
+  // drop out rate of primary education
+  DROP_OUT_RATE_OF_PRIMARY_EDUCATION_TOTAL_2018_2018:
+    "https://api.uis.unesco.org/sdmx/data/UNESCO,EDU_NON_FINANCE,3.0/DR.PT.L1.._T.................?startPeriod=2018&endPeriod=2018&format=sdmx-json&locale=en&subscription-key=",
+  DROP_OUT_RATE_OF_PRIMARY_EDUCATION_FEMALE_2018_2018:
+    "https://api.uis.unesco.org/sdmx/data/UNESCO,EDU_NON_FINANCE,3.0/DR.PT.L1..F.................?startPeriod=2018&endPeriod=2018&format=sdmx-json&locale=en&subscription-key=",
+  DROP_OUT_RATE_OF_PRIMARY_EDUCATION_MALE_2018_2018:
+    "https://api.uis.unesco.org/sdmx/data/UNESCO,EDU_NON_FINANCE,3.0/DR.PT.L1..M.................?startPeriod=2018&endPeriod=2018&format=sdmx-json&locale=en&subscription-key=",
+};
