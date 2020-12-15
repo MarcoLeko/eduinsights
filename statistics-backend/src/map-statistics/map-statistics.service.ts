fcbc1343b91f1aa2a49bd49ed5bc93225fb644f8
Marco Leko <marco.leko@jsmd-group.com>
2020-12-15 13:12:02 +0100

    add first handling of map list in frontend/backend -  still todos left

--- statistics-backend/src/map-statistics/map-statistics.service.ts
+++ statistics-backend/src/map-statistics/map-statistics.service.ts
@@ -1,17 +1,44 @@
 import { Injectable } from '@nestjs/common';
-import { InjectConnection, InjectModel } from '@nestjs/mongoose';
-import { Connection, Model } from 'mongoose';
-import { MapStatistics, MapStatisticsDocument } from './map-statistics.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import {
+  MapStatistics,
+  MapStatisticsDocument,
+  MapStatisticsList,
+  MapStatisticsListDocument,
+} from './map-statistics.schema';
 
 @Injectable()
 export class MapStatisticsService {
   constructor(
     @InjectModel(MapStatistics.name)
     private readonly mapStatisticsModel: Model<MapStatisticsDocument>,
-    @InjectConnection() private connection: Connection,
+    @InjectModel(MapStatisticsList.name)
+    private readonly mapStatisticsListModel: Model<MapStatisticsListDocument>,
   ) {}
 
   getMapStatisticsById(type: string): Promise<MapStatisticsDocument[]> {
     return this.mapStatisticsModel.find({ type }).exec();
+  }
+
+  // TODO: get actual db collection
+  // Check why collections like: DROP_OUT_RATE_OF_PRIMARY_EDUCATION_FEMALE_2018_201 has everywhere 0 set
+  getMapStatisticsList(): Promise<MapStatisticsListDocument[]> {
+    return {
+      type: 'list',
+      statistics: [
+        'DROP_OUT_RATE_OF_PRIMARY_EDUCATION_FEMALE_2018_2018',
+        'DROP_OUT_RATE_OF_PRIMARY_EDUCATION_MALE_2018_2018',
+        'DROP_OUT_RATE_OF_PRIMARY_EDUCATION_TOTAL_2018_2018',
+        'GROSS_ENROLMENT_RATE_FOR_PRIMARY_EDUCATION_2018_2018',
+        'PERCENTAGE_OF_GRADUATES_BACHELORS_FEMALE_2018_2018',
+        'PERCENTAGE_OF_GRADUATES_BACHELORS_TOTAL_2018_2018',
+        'PERCENTAGE_OF_GRADUATES_DOCTORAL_FEMALE_2018_2018',
+        'PERCENTAGE_OF_GRADUATES_DOCTORAL_TOTAL_2018_2018',
+        'PROPORTION_OF_PRIMARY_SCHOOLS_INTERNET_ACCESS_2018_2018',
+      ],
+    } as any;
+
+    // return this.mapStatisticsListModel.find({}).exec();
   }
 }
