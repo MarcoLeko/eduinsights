3f7734ebcb5d5e1563010df3abc0233d14b28f83
Marco Leko <marco.leko@jsmd-group.com>
2020-12-18 15:02:29 +0100

    change order of map statistics

--- tools/scripts/db/map-statistics.js
+++ tools/scripts/db/map-statistics.js
@@ -1,5 +1,21 @@
 module.exports = mapStatistics = [
   {
+    description: "Net enrolment rate for primary education",
+    key: "NET_ENROLMENT_RATE_FOR_PRIMARY_EDUCATION_2018_2018",
+    url:
+      "https://api.uis.unesco.org/sdmx/data/UNESCO,EDU_NON_FINANCE,3.0/NER.PT.L1...................?startPeriod=2018&endPeriod=2018&format=sdmx-json&locale=en&subscription-key=",
+    startYear: 2018,
+    endYear: 2018,
+    evaluationType: "percentage",
+    evaluation: [
+      { key: "firstRange", value: [100, 98] },
+      { key: "secondRange", value: [95, 90] },
+      { key: "thirdRange", value: [89, 85] },
+      { key: "fourthRange", value: [84, 70] },
+      { key: "fifthRange", value: [49, 0] },
+    ],
+  },
+  {
     description:
       "Proportion of primary schools with access to internet for pedagogical purposes",
     key: "PROPORTION_OF_PRIMARY_SCHOOLS_INTERNET_ACCESS_2018_2018",
@@ -14,22 +30,6 @@ module.exports = mapStatistics = [
       { key: "thirdRange", value: [49, 25] },
       { key: "fourthRange", value: [25, 10] },
       { key: "fifthRange", value: [10, 0] },
-    ],
-  },
-  {
-    description: "Net enrolment rate for primary education",
-    key: "NET_ENROLMENT_RATE_FOR_PRIMARY_EDUCATION_2018_2018",
-    url:
-      "https://api.uis.unesco.org/sdmx/data/UNESCO,EDU_NON_FINANCE,3.0/NER.PT.L1...................?startPeriod=2018&endPeriod=2018&format=sdmx-json&locale=en&subscription-key=",
-    startYear: 2018,
-    endYear: 2018,
-    evaluationType: "percentage",
-    evaluation: [
-      { key: "firstRange", value: [100, 95] },
-      { key: "secondRange", value: [94, 89] },
-      { key: "thirdRange", value: [88, 80] },
-      { key: "fourthRange", value: [79, 50] },
-      { key: "fifthRange", value: [49, 0] },
     ],
   },
   {
