21d597fd794306012dd605f1532c24013ade2c08
Marco Leko <marco.leko@jsmd-group.com>
2020-12-21 23:09:58 +0100

    add screen lock feature

--- statistics-frontend/src/index.jsx
+++ statistics-frontend/src/index.jsx
@@ -22,3 +22,7 @@ ReactDOM.render(<Bootstrap />, document.getElementById("root"));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+window.screen.orientation
+  .lock("portrait")
+  .then(() => console.log("Orientation successfully locked to portrait mode"))
+  .catch((e) => console.log("Cannot lock orientation: ", e));
