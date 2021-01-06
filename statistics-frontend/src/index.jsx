8703d62d971bd7e7a58da4ae709521f021fafa8d
Marco Leko <marco.leko@jsmd-group.com>
2021-01-06 13:43:49 +0100

    unregister service worker

--- statistics-frontend/src/index.jsx
+++ statistics-frontend/src/index.jsx
@@ -29,7 +29,7 @@ ReactGA.pageview(window.location.pathname + window.location.search);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
