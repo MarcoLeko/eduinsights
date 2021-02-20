92a6a63d4296090a7dbb144826d1b13e6b577784
Marco Leko <marco.leko@jsmd-group.com>
2021-02-20 12:48:01 +0100

    added image for app-description

--- statistics-frontend/src/components/app-description-modal/app-description-modal.jsx
+++ statistics-frontend/src/components/app-description-modal/app-description-modal.jsx
@@ -12,8 +12,36 @@ import {
 } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import "./app-description-modal.scss";
+import StatisticImage from "../../assets/map-transparent-bg.png";
 
 const cookieKey = "cookieLawAccepted";
+const dialogTitles = ["How it works", "Select a visualization", "Enjoy!"];
+
+const CookieContent = () => (
+  <>
+    <DialogContentText id="dialog-description" align={"justify"}>
+      Hover over the states and compare the observations. In addition, you can
+      access the statistic via url or the recent queries panel.
+    </DialogContentText>
+    <img src={StatisticImage} className="dialog-image" />
+    <DialogContentText id="dialog-description" align={"justify"}>
+      To enable you the best surfing experience, we are using cookies. By
+      proceeding you agree on our cookie policy{" "}
+      <span role="img" aria-labelledby="cookie">
+        🍪
+      </span>
+    </DialogContentText>
+  </>
+);
+
+const SelectStatisticContent = () => <></>;
+const SelectVisualizationContent = () => <></>;
+
+const dialogContent = [
+  SelectStatisticContent(),
+  SelectVisualizationContent(),
+  CookieContent(),
+];
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
@@ -22,11 +50,17 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 export function AppDescriptionModal() {
   const [item, setValue] = useCookie(cookieKey);
   const [open, setOpen] = useState(!item);
+  const [step, setStep] = useState(2);
   const theme = useTheme();
   const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
   const handleClose = () => {
     setOpen(false);
     setValue(true);
+  };
+
+  const handleNext = () => {
+    setStep(step + 1);
   };
 
   return (
@@ -40,24 +74,19 @@ export function AppDescriptionModal() {
       aria-labelledby="dialog-title"
       aria-describedby="dialog-description"
     >
-      <DialogTitle id="dialog-title">Eduinsights - how it works</DialogTitle>
-      <DialogContent>
-        <DialogContentText id="dialog-description">
-          To enable you the best surfing experience, we are using cookies. By
-          proceeding you agree on our cookie policy{" "}
-          <span role="img" aria-labelledby="cookie">
-            🍪
-          </span>
-        </DialogContentText>
-      </DialogContent>
+      <DialogTitle id="dialog-title">
+        Eduinsights - {dialogTitles[step]}
+      </DialogTitle>
+      <DialogContent>{dialogContent[step]}</DialogContent>
+
       <DialogActions>
         <Button
-          onClick={handleClose}
+          onClick={step < dialogTitles.length - 1 ? handleNext : handleClose}
           color="primary"
           variant="contained"
           className="primary-button"
         >
-          Proceed
+          {step < dialogTitles.length - 1 ? "Proceed" : "Accept"}
         </Button>
       </DialogActions>
     </Dialog>
